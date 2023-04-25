8a5af6a9cabc7a4b45545e4133bdc4d25e68db81
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-04-25 12:08:35 +0100

    change callable gateway

--- src/main/main_pipeline.py
+++ src/main/main_pipeline.py
@@ -1,7 +1,7 @@
 from ..stages.load.load_data import LoadData
 from ..stages.transform.transform_raw_data import TransformRawData
 from ..stages.extract.extract_questionnaires import ExtracQuestionnairesStage
-from ..stages.contracts.mocks.extract_contract_mock import extract_contract_mock
+# from ..stages.contracts.mocks.extract_contract_mock import extract_contract_mock
 
 from ..drivers.http_requester import HttpRequester
 from ..gateway.questionnaire_gateway import QuestionnaireGateway
@@ -15,15 +15,15 @@ from ..services.implementation.questions_implementation import QuestionsImplemen
 class MainPipeline:
     def __init__(self) -> None:        
         self.__extract_stage = ExtracQuestionnairesStage(
-            HttpRequester(QuestionnaireGateway(10))
+            HttpRequester(QuestionnaireGateway)
             )
         self.__transform_raw_data = TransformRawData()
         self.__load_data = LoadData(AnswersImplementation(), QuestionsImplementation())
 
-    def run_pipeline(self) -> None:
+    async def run_pipeline(self) -> None:
         """run pupeline"""
         TableStatements.create_table()
-        # extract_contract = self.__extract_stage.extract()
+        extract_contract = await self.__extract_stage.extract()
         transformed_data_contract = self.__transform_raw_data\
-                                        .transform(extract_contract_mock)
+                                        .transform(extract_contract)
         self.__load_data.load(transformed_data_contract)
