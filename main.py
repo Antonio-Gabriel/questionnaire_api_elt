569fc924f5b4717c6a01c9a38e9a184d3993206c
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-04-25 11:30:21 +0100

    start types cli

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import uvicorn, typer
 from src.main import main_pipeline
+from src.main.server import app
+
+typer_cli = typer.Typer()
+
+@typer_cli.command("server")
+def http_server():
+    """start appication server"""
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
+
+
+@typer_cli.command("pipeline")
+def run_pipeline():
+    """start pipeline process"""
+    main_pipeline.run_pipeline()
+
 
 if __name__ == "__main__":
-    main_pipeline.run_pipeline()
+    typer_cli()
