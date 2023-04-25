6d2c3a4cb35a3a1187a9ea2bc453642fc2ebd3b2
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-04-25 11:29:49 +0100

    create answers mapper

---
+++ src/main/mappers/answer_mapper.py
@@ -0,0 +1,16 @@
+from ..models.answer_model import AnswerModel, AnswerMapper
+
+def answer_mapper(answers: list):
+    """answers mapper response"""
+    answers_mapped = []
+
+    for question in answers:
+        answer_model = AnswerModel(
+            id=question[0], question_id=question[1], text=question[2],
+            is_correct=question[3], created_at=question[4])
+        
+        answer_bind = AnswerMapper.build_from_model(answer_model)
+
+        answers_mapped.append(answer_bind.dict())
+        
+    return answers_mapped
