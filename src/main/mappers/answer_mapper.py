671c6e9db9233ebc0b84bc8e450de64377d89fc2
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-04-25 12:23:35 +0100

    change answer mapper

--- src/main/mappers/answer_mapper.py
+++ src/main/mappers/answer_mapper.py
@@ -6,7 +6,7 @@ def answer_mapper(answers: list):
 
     for question in answers:
         answer_model = AnswerModel(
-            id=question[0], question_id=question[1], text=question[2],
+            id=question[0], question_id=question[2], text=question[1],
             is_correct=question[3], created_at=question[4])
         
         answer_bind = AnswerMapper.build_from_model(answer_model)
