9622eb50a7098cf4d9ac3c66347d0e0ac956e86a
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-04-25 09:28:35 +0100

    set db global environment into db

--- src/database/database_connetor.py
+++ src/database/database_connetor.py
@@ -1,8 +1,8 @@
 import sqlite3, os
 
-class DatabaseConnector:
+class DatabaseConnector:    
     @staticmethod
     def get_connection():
         """database connection instance"""
-        con = sqlite3.connect(os.environ["URL_BASE"])
+        con = sqlite3.connect(os.environ["DB_NAME"])
         return con
